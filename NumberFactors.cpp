19f1382c12098fde2334c44b34ba1b5c8e93a8d5
Kim <109536254+KimCoterwas@users.noreply.github.com>
2022-07-20 12:01:01 -0400

    Adding Chapter 3 Challenge #2

---
+++ NumberFactors.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int num;
+	cout << "Enter a positive integer: ";
+	cin >> num;
+
+	cout << "Factors of " << num << " are: ";
+	for (int i = 1; i <= num; i++) {
+		if (num % i == 0)
+			cout << i << " ";
+	}
+}
